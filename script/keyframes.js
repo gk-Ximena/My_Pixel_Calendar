87bd254baa2e933923e138237a388b34dd48ea85
Ximena <ximenalvarezmontoya@gmail.com>
2026-01-15 00:10:40 -0800

    UI computer integration Interface for computers for index page

---
+++ script/keyframes.js
@@ -0,0 +1,26 @@
+//General keyframes
+
+//Menu keyframe animation
+let menuBtn = document.getElementById("menu");
+
+menuBtn.style.position = "absolute";
+
+let menuMovementKeyFrames = [
+    { top: "0%", left: "0%", offset: 0 },
+    { top: "0%", left: "-50%", offset: 0.7 },
+    { top: "-10%", left: "0%", offset: 0.85 },
+    { top: "0%", left: "0%", offset: 1 }
+];
+
+let menuMovementProperties ={
+    duration: 2000,
+    fill: "forwards"
+}
+
+let menuAnimation = menuBtn.animate(menuMovementKeyFrames, menuMovementProperties);
+
+menuAnimation.pause();
+
+menuBtn.addEventListener("click", function(event){
+    menuAnimation.play();
+});
